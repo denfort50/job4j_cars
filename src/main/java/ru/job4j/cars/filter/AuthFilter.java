0b5b7b49b2b6bad20bfc022f9d75471c7ae423ee
denfort50 <denfort50@yandex.ru>
2023-01-07 10:00:57 +0300

    Добавление фильтра

---
+++ src/main/java/ru/job4j/cars/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package ru.job4j.cars.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class AuthFilter implements Filter {
+
+    private final Set<String> mappings =
+            new HashSet<>(Set.of("loginPage", "addUser", "registration", "login", "fail", "success"));
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (mappingIsPresent(uri) || showPhotosWithoutLogin(uri)) {
+            chain.doFilter(req, res);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "users/loginPage");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+
+    private boolean mappingIsPresent(String uri) {
+        return mappings.stream().anyMatch(uri::endsWith);
+    }
+
+    private boolean showPhotosWithoutLogin(String uri) {
+        return uri.contains("/postPhoto/");
+    }
+
+}
