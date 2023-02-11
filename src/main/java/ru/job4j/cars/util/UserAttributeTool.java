d0485fa72a1fbc5b6a73e59c5f8df273b9164345
denfort50 <denfort50@yandex.ru>
2023-02-11 15:50:53 +0300

    Обновлен утилитный класс

--- src/main/java/ru/job4j/cars/util/UserAttributeTool.java
+++ src/main/java/ru/job4j/cars/util/UserAttributeTool.java
@@ -5,12 +5,20 @@ import ru.job4j.cars.model.User;
 
 import javax.servlet.http.HttpSession;
 
+/**
+ * Утилитный класс для добавления в модель атрибута - пользователь
+ */
 public final class UserAttributeTool {
 
     private UserAttributeTool() {
         throw new UnsupportedOperationException("This is utility class and it cannot be instantiated");
     }
 
+    /**
+     * Метод добавляет атрибут - пользователя в модель
+     * @param model модель
+     * @param httpSession сессия
+     */
     public static void addAttributeUser(Model model, HttpSession httpSession) {
         User user = getAttributeUser(httpSession);
         if (user == null) {
@@ -20,6 +28,11 @@ public final class UserAttributeTool {
         model.addAttribute("user", user);
     }
 
+    /**
+     * Метод получает атрибут - пользователя
+     * @param httpSession сессия
+     * @return возвращает пользователя
+     */
     public static User getAttributeUser(HttpSession httpSession) {
         return (User) httpSession.getAttribute("user");
     }
