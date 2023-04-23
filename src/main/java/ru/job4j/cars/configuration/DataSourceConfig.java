0978abb8457406dad466fafbf73d0b6427e18b5e
denfort50 <denfort50@yandex.ru>
2023-04-23 19:35:43 +0300

    Configuration update

--- src/main/java/ru/job4j/cars/configuration/DataSourceConfig.java
+++ src/main/java/ru/job4j/cars/configuration/DataSourceConfig.java
@@ -1,13 +1,19 @@
 package ru.job4j.cars.configuration;
 
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.AllArgsConstructor;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import ru.job4j.cars.repository.CrudRepository;
 import ru.job4j.cars.repository.CrudRepositoryImpl;
+
+import javax.sql.DataSource;
 
 /**
  * Класс представляет собой конфигурацию ORM Hibernate
@@ -15,7 +21,10 @@ import ru.job4j.cars.repository.CrudRepositoryImpl;
  * @version 1.0
  */
 @Configuration
+@AllArgsConstructor
 public class DataSourceConfig {
+
+    private final Environment environment;
 
     /**
      * Метод создает объект SessionFactory для многократного использования в приложении
@@ -35,5 +44,23 @@ public class DataSourceConfig {
     @Bean
     public CrudRepository getCrudRepository() {
         return new CrudRepositoryImpl(getSessionFactory());
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource pool = new BasicDataSource();
+        pool.setDriverClassName(environment.getProperty("db.datasource.driver"));
+        pool.setUrl(environment.getProperty("db.datasource.url"));
+        pool.setUsername(environment.getProperty("db.datasource.username"));
+        pool.setPassword(environment.getProperty("db.datasource.password"));
+        return pool;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(environment.getProperty("db.datasource.changeLogFile"));
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
     }
 }
--- src/test/java/ru/job4j/cars/configuration/TestDataSourceConfig.java
+++ src/test/java/ru/job4j/cars/configuration/TestDataSourceConfig.java
@@ -1,13 +1,24 @@
 package ru.job4j.cars.configuration;
 
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.AllArgsConstructor;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import ru.job4j.cars.Main;
 import ru.job4j.cars.repository.CrudRepository;
 import ru.job4j.cars.repository.CrudRepositoryImpl;
+
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * Класс представляет собой конфигурацию ORM Hibernate
@@ -15,7 +26,10 @@ import ru.job4j.cars.repository.CrudRepositoryImpl;
  * @version 1.0
  */
 @Configuration
+@AllArgsConstructor
 public class TestDataSourceConfig {
+
+
 
     /**
      * Метод создает объект SessionFactory для многократного использования в приложении
@@ -31,5 +45,51 @@ public class TestDataSourceConfig {
     @Bean
     public CrudRepository getCrudRepository() {
         return new CrudRepositoryImpl(getSessionFactory());
+    }
+
+    private Properties loadDbProperties() {
+        Properties cfg = new Properties();
+        try (BufferedReader io = new BufferedReader(
+                new InputStreamReader(
+                        Objects.requireNonNull(Main.class.getClassLoader()
+                                .getResourceAsStream("application.properties"))
+                )
+        )) {
+            cfg.load(io);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            Class.forName(cfg.getProperty("db.test.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return cfg;
+    }
+
+    private Properties getProperties() {
+        return loadDbProperties();
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        Properties cfg = getProperties();
+        BasicDataSource pool = new BasicDataSource();
+        pool.setDriverClassName(cfg.getProperty("db.test.driver"));
+        pool.setUrl(cfg.getProperty("db.test.url"));
+        pool.setUsername(cfg.getProperty("db.test.username"));
+        pool.setPassword(cfg.getProperty("db.test.password"));
+        pool.setMinIdle(5);
+        pool.setMaxIdle(10);
+        pool.setMaxOpenPreparedStatements(100);
+        return pool;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(getProperties().getProperty("db.test.changeLogFile"));
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
     }
 }
